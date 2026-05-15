99b13200c5b99444c652f2a79d2a528a54a575cf
Dmytro <dimasdaniuk@gmail.com>
2026-05-15 18:59:14 +0300

    Added Logging, @keyword decorator and type hinting in VideoGave Library

--- VideoGameApi/VideoGamesLib.py
+++ VideoGameApi/VideoGamesLib.py
@@ -1,12 +1,16 @@
 import random
+from typing import Dict, Any
 from jsonschema import validate, ValidationError
+
+from robot.api.deco import keyword
+from robot.api import logger
 
 
 class VideoGamesLib:
-
-    def generate_random_game_payload(self):
+    @keyword("Generate Random Game Payload")
+    def generate_random_game_payload(self) -> Dict[str, Any]:
         game_id = random.randint(1000, 9999)
-        return {
+        payload = {
             "id": game_id,
             "name": f"AutoTest Game {game_id}",
             "releaseDate": "2024-05-14",
@@ -15,7 +19,12 @@ class VideoGamesLib:
             "rating": "Mature"
         }
 
-    def validate_game_schema(self, response_json):
+        logger.info(f"Generated new data of game: {payload}")
+
+        return payload
+
+    @keyword("Validate Game Schema")
+    def validate_game_schema(self, response_json: Dict[str, Any]) -> None:
         schema = {
             "type": "object",
             "properties": {
@@ -28,7 +37,11 @@ class VideoGamesLib:
             },
             "required": ["id", "name", "category"]
         }
+
         try:
             validate(instance=response_json, schema=schema)
+            logger.info("JSON Schema successfully validated")
+
         except ValidationError as e:
+            logger.error(f"Error of Validations: {e.message}")
             raise AssertionError(f"Помилка валідації JSON Schema: {e.message}")
